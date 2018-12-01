e365a0aeadfea42bae9d8df6ba905bf47932af06
efarmer <ericdfarmer988@gmail.com>
2018-12-01 15:17:21 -0600

    Normalized audio levels

--- src/matlab/normalize_outputs.m
+++ src/matlab/normalize_outputs.m
@@ -30,11 +30,11 @@ for i = 1:2:length(locations(:,1))
     for j = 1:2:length(input_samples_files(:,1))
         [real_output, fs_real] = audioread(strcat(output_path_real, locations(i,:), input_samples_files(j+1,:)));
         real_output_normalized = real_output ./ max(abs(real_output));
-        audiowrite(strcat(output_path_real, locations(i,:), input_samples_files(j+1,:)), real_output, fs_real, 'BitsPerSample', 32);
+        audiowrite(strcat(output_path_real, locations(i,:), input_samples_files(j+1,:)), real_output_normalized, fs_real, 'BitsPerSample', 32);
 
         [sim_output, fs_sim] = audioread(strcat(output_path_sim, locations(i,:), input_samples_files(j+1,:)));
         sim_output_normalized = sim_output ./ max(abs(sim_output));
-        audiowrite(strcat(output_path_sim, locations(i,:), input_samples_files(j+1,:)), sim_output, fs_sim, 'BitsPerSample', 32);
+        audiowrite(strcat(output_path_sim, locations(i,:), input_samples_files(j+1,:)), sim_output_normalized, fs_sim, 'BitsPerSample', 32);
     end
 end
 more on;
